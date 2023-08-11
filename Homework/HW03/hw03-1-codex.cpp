0c73d5af8edb06600008917fd3f06e1ff0cbfca0
porameter <paramet_su66@live.rmutl.ac.th>
2023-08-11 21:11:29 +0700

    rewrewrewrew

--- Homework/HW03/hw03-1-codex.cpp
+++ Homework/HW03/hw03-1-codex.cpp
@@ -1,20 +1,20 @@
 //TODO: #8 Arrange this C coding into CODEX pattern.
 #include <stdio.h>
-#include <conio.h> // This header is specific to Windows and may not be available on all systems.
-
+#include <conio.h>
 int main() {
-    int a = 10;
-    int b = 20, c = 30, d = 40;
+    int a = 10 ;
+    int b = 20 ; 
+    int d = 40 ;
 
-    printf("This is %d + %d = %d ", a, b, a + b);
+    printf("This is %d + %d = %d ", a, b, a + b) ;
     
     for (int i = 0; i < d - 30; i++) {
         if (i < 30) {
-            printf("Here");
+            printf( "Here") ;
         } else if (i == 8) {
-            printf("This is 8.");
+            printf( "This is 8.") ;
         } else {
-            printf("ok");
+            printf( "ok") ;
         }
     }
 
